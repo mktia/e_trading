8d0a4e1cf8e62ba7ac4d06decda9c78a4dd755c4
mktia <endoplasmicreticulum2468741059@gmail.com>
2019-09-06 01:29:00 +0900

    電力損失の計算の追加と修正

--- notrading.py
+++ notrading.py
@@ -29,30 +29,58 @@ with open(f'./{input_file}', newline='') as f:
         values = [float(i) for i in row]
         random_consumption.append(np.array(values))
 
-label = ['time'] + [f'nd{network_index}/{node_index + 1}'
-                    for network_index in range(world_size) for node_index in range(network_edge) for i in range(5)]
+
+def pickup_surplus(electricity):
+    """
+    電力損失が発生する電圧の電力のみを抽出
+
+    Args
+        electricity (numpy.ndarray): ノードの余剰電力量
+    """
+    loss = np.zeros(electricity.size)
+    for id, item in enumerate(electricity):
+        if item > 0:
+            loss[id] = item
+
+    return loss
+
+
+# 変数名の省略
+nws = world_size
+nds = network_edge
+label = ['time'] + [f'nd{nw}/{nd + 1}' for nw in range(nws) for nd in range(nds) for i in range(5)] + [f'ls{nw}/{nd + 1}' for nw in range(
+    nws) for nd in range(nds)] + [f'ls_cml{nw}/{nd + 1}' for nw in range(nws) for nd in range(nds)]
 
 el_amount = [[np.array(random_consumption.pop(0))
               for i in range(network_edge)] for j in range(world_size)]
+ls_amount = [[0 for nd in range(network_edge)] for nw in range(world_size)]
+ls_cumul_amount = [[0 for nd in range(network_edge)]
+                   for nw in range(world_size)]
 
 output = [label]
-output.append(
-    ['0'] + [el_item for el_network in el_amount for el_node in el_network for el_item in el_node])
+output.append(['0'] + [el_item for el_network in el_amount for el_node in el_network for el_item in el_node] +
+              [item for nw in ls_amount for item in nw] + [item for nw in ls_cumul_amount for item in nw])
+
 
 for i in range(simulation_length):
     time = i + 1
 
     if time % 10 == 0:
-
-        for el_network in el_amount:
-            for el_node in el_network:
+        for nw, el_network in enumerate(el_amount):
+            for nd, el_node in enumerate(el_network):
+                # 損失した電力量
+                loss_in_node = pickup_surplus(el_node) * (1 - efficiency)
+                # 瞬間的な電力損失の記録
+                ls_amount[nw][nd] = np.sum(loss_in_node)
+                # 累積の電力損失の記録
+                ls_cumul_amount[nw][nd] += np.sum(loss_in_node)
                 # 自然放電
-                el_node *= efficiency
+                el_node -= loss_in_node
                 # 電力の消費生産
                 el_node += random_consumption.pop(0)
 
-    output.append(
-        [time] + deepcopy([el_item for el_network in el_amount for el_node in el_network for el_item in el_node]))
+    output.append([time] + deepcopy([item for el_network in el_amount for el_node in el_network for item in el_node]) +
+                  deepcopy([item for nw in ls_amount for item in nw])+deepcopy([item for nw in ls_cumul_amount for item in nw]))
 
 
 print('Saving...')
